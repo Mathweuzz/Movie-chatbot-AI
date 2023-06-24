422ad7394059347126b9fd43e972bfcc7775e82d
yantavares <yantdo1@gmail.com>
2023-06-24 00:15:28 -0300

    added flask and django api

--- api.py
+++ api.py
@@ -1,24 +1,30 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify, Response
 import numpy as np
 import pandas as pd
 from chatbot import get_response
 from movie_rec_routine import movie_rec
+from flask_cors import CORS
 
 similarity = pd.read_parquet('similarity', engine='pyarrow')
 similarity = np.array(similarity)
 
 app = Flask(__name__)
 
-@app.route('/getResponse')
-def getResponse():
+CORS(app)
+
+
+@app.route('/data', methods=['GET'])
+def data():
     text = request.args.get('text')
     if text[:6].lower() == 'movie:':
         message = {'answer': movie_rec(text[6:], similarity)}
     else:
         response = get_response(text)
         message = {'answer': response}
+
     return message
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
